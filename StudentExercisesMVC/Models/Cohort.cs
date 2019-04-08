e2653ab34635fdeffc799dcc932b7e94e63396a2
Allison Collins <allisonkatecollins@gmail.com>
2019-04-08 15:20:19 -0500

    fixed file names

--- StudentExercisesMVC/Models/Cohort.cs
+++ StudentExercisesMVC/Models/Cohort.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace StudentExercisesAPI.Models
+namespace StudentExercisesMVC.Models
 {
     public class Cohort
     {
